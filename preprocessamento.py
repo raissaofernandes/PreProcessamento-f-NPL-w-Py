e704768b0388a71f5282d8b76bc0a51df8e906af
Raissa O. Fernandes <44683941+rsshnf@users.noreply.github.com>
2020-03-28 14:14:01 -0300

    Update preprocessamento.py

--- preprocessamento.py
+++ preprocessamento.py
@@ -1,57 +1,33 @@
 import nltk
 import unicodedata
-import codecs
-#import emoji IMPLEMENTAR ISSO DEPOIS!!!
-from nltk.stem import RSLPStemmer
 
-def normalize(sentence):
-    sentence = sentence.lower()
-    sentence = strip_accents(sentence)
-    sentence = nltk.word_tokenize(sentence)
-    return sentence
+#declaração das stopwords que serão retiradas dos tuítes
+stopwords = ['pra', 'pro','to', 'ta','de', 'a', 'o', 'que', 'e', 'do', 'da', 'em', 'um', 'para', 'a', 'com', 'nao', 'uma', 'os', 'no', 'se', 'na', 'por', 'mais', 'as', 'dos', 'como', 'mas', 'foi', 'ao', 'ele', 'das', 'tem', 'a', 'seu', 'sua', 'ou', 'ser', 'quando', 'muito', 'ha', 'nos', 'ja', 'esta', 'eu', 'tambam', 'sa3', 'pelo', 'pela', 'ata', 'isso', 'ela', 'entre', 'era', 'depois', 'sem', 'mesmo', 'aos', 'ter', 'seus', 'quem', 'nas', 'me', 'esse', 'eles', 'estao', 'vocaa', 'tinha', 'foram', 'essa', 'num', 'nem', 'suas', 'meu', 'a s', 'minha', 'taam', 'numa', 'pelos', 'elas', 'havia', 'seja', 'qual', 'sera', 'na3s', 'tenho', 'lhe', 'deles', 'essas', 'esses', 'pelas', 'este', 'fosse', 'dele', 'tu', 'te', 'vocaas', 'vos', 'lhes', 'meus', 'minhas', 'teu', 'tua', 'teus', 'tuas', 'nosso', 'nossa', 'nossos', 'nossas', 'dela', 'delas', 'esta', 
+'estes', 'estas', 'aquele', 'aquela', 'aqueles', 'aquelas', 'isto', 'aquilo', 'estou', 'esta', 'estamos', 'estao', 'estive', 'esteve', 'estivemos', 'estiveram', 'estava', 'estavamos', 'estavam', 'estivera', 'estivaramos', 'esteja', 'estejamos', 'estejam', 'estivesse', 'estivassemos', 'estivessem', 'estiver', 'estivermos', 'estiverem', 'hei', 'ha', 'havemos', 'hao', 'houve', 'houvemos', 'houveram', 'houvera', 'houvaramos', 'haja', 'hajamos', 'hajam', 'houvesse', 'houvassemos', 'houvessem', 'houver', 'houvermos', 'houverem', 'houverei', 'houvera', 'houveremos', 'houverao', 'houveria', 'houveraamos', 'houveriam', 'sou', 'somos', 'sao', 'era', 'aramos', 'eram', 'fui', 'foi', 'fomos', 'foram', 'fora', 'fa ramos', 'seja', 'sejamos', 'sejam', 'fosse', 'fa ssemos', 'fossem', 'for', 'formos', 'forem', 'serei', 'sera', 'seremos', 'serao', 'seria', 'seraamos', 'seriam', 'tenho', 'tem', 'temos', 'tam', 'tinha', 'tanhamos', 'tinham', 'tive', 'teve', 'tivemos', 'tiveram', 'tivera', 'tivaramos', 'tenha', 'tenhamos', 'tenham', 'tivesse', 'tivassemos', 'tivessem', 'tiver', 'tivermos', 'tiverem', 'terei', 'tera', 'teremos', 'terao', 'teria', 'teraamos', 'teriam']
+print(len(stopwords))
 
-def tokenize(sentence):
-    sentence = sentence.lower()
-    sentence = nltk.word_tokenize(sentence)
-    return sentence
-
-def strip_accents(sentence):
-    try:
-        sentence = unicode(sentence, 'utf-8')
-    except NameError: # unicode is a default on python 3 
-        pass
-
-    sentence = unicodedata.normalize('NFD', sentence)\
-           .encode('ascii', 'ignore')\
-           .decode("utf-8")
-    return str(sentence)
-
+#função para a remoção das stopwords
 def remove_stopWords(sentence):
-    stopwords = nltk.corpus.stopwords.words('portuguese')
-    phrase = []
-    for word in sentence:
+    frase = []
+    for word in sentence.split():
         if word not in stopwords:
-            phrase.append(word)
-    return phrase
+           # semStop = [p for p in word.split() if p not in stopwords]
+            frase.append(word)
+    return ' '.join(frase)
 
-def stemming(sentence):
-    stemmer = RSLPStemmer()
-    phrase = []
-    for word in sentence:
-        phrase.append(stemmer.stem(word.lower()))
-    return phrase
+def removerAcentosECaracteresEspeciais(palavra):
 
-  #Remover caracteteres irrelevantes como os não alfanuméricos;
-  #Remover palavras que não são relevantes, como as menções indicadas com "@";
-  #Converter termos para minúsculos, homogeneizando os dados (OLÁ - olá);
-  #Combinar palavras escritas incorretamente para uma única representação (legaaalll - legal);
-  #Considerar lematização (reduzir palavras como "sou", "é" para uma forma desflexionada como "ser")
-  #Quebrar o seu texto em termos.
-def limpeza_dos_dados(df, tuites):
-    df[tuites] = df[tuites].str.replace(r"http\S+", "")
-    df[tuites] = df[tuites].str.replace(r"http", "")
-    df[tuites] = df[tuites].str.replace(r"@\S+", "")
-    df[tuites] = df[tuites].str.replace(r"[^A-Za-z0-9(),!?@\'\`\"\_\n]", " ")
-    df[tuites] = df[tuites].str.replace(r"@", "at")
-    df[tuites] = df[tuites].str.lower()
-    return df
+    nfkd = unicodedata.normalize('NFKD', palavra)
+    palavraSemAcento = u"".join([c for c in nfkd if not unicodedata.combining(c)])
+    palavraSemAcento = re.sub('[^a-zA-Z0-9 \\\]', '', palavraSemAcento)
+
+    return palavraSemAcento
+
+#recebe os tuítes e a coluna Text
+def limpeza_dados(tuites, text_field):
+    tuites[text_field] = tuites[text_field].str.lower() #coloca todo o tuíte em letras minúsculas
+    tuites[text_field] = tuites[text_field].str.replace(r"#", " ") #remove hashtags
+    tuites[text_field] = tuites[text_field].str.replace(r"http\S+", " ") #remove os links
+    tuites[text_field] = tuites[text_field].str.replace(r"@", "at") #remove os arrobas e substitui por at
+    tuites[text_field] = tuites[text_field].str.replace(r"\n", " ") #remove as linhas em branco
+    return tuites
